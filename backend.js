7051d0602b0e9d86623c3d6ee02bf1f99735e418
Pyramid30 <151288894+Pyramid302614@users.noreply.github.com>
2026-09-11 13:38:44 -0700

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,42 @@
+const server = require("http").createServer((req,res) => {
+
+
+});
+server.listen("0.0.0.0",require("./config.json").port,() => console.log("Ready when you are"));
+const wss = new (require("ws").WebSocket).Server({ server });
+wss.on("connection",ws => {
+   ws.on("message",(m) => {
+       const msg = m.toString();
+       const name = msg.split(":")[0];
+       const args = msg.split(":").slice(1);
+       switch(name) {
+           case "fetch": ws.send(`fetch-resp:${getProject(args[0])}`); break;
+           case "push": updateProject(args[0],args[1]); break;
+           case "new": newProject(args[0]); break;
+           case "snap": snapProject(args[0],args[1]); break;
+       }
+   });
+});
+
+
+const fs = require("fs");
+
+
+function newProject(name) {
+   if(!fs.existsSync("projects")) {
+       fs.mkdirSync("projects");
+   }
+   fs.writeFileSync(`projects/${name}.js`,"");
+}
+function updateProject(name, contents) {
+   fs.writeFileSync(`projects/${name}.js`,contents);
+}
+function snapProject(name, contents) {
+   // fs.writeFileSync(`projects/${name}.snap.js`,`// p6 project created at ${Date.now()}\n\n${contents}`);
+}
+function getProject(name, contents) {
+   return fs.readFileSync(`projects/${name}.js`);
+}
+
+
+
